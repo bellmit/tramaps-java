e2602ed2ce12823151d0ae5b792305a055aaf5a0
Thomas Zuberbühler <thomas.zuberbuehler@gmail.com>
2016-08-21 15:07:40 +0200

    drawing

--- src/main/java/ch/geomo/tramaps/MainScaleApp.java
+++ src/main/java/ch/geomo/tramaps/MainScaleApp.java
@@ -2,12 +2,8 @@ package ch.geomo.tramaps;
 
 import ch.geomo.tramaps.conflicts.Conflict;
 import ch.geomo.tramaps.conflicts.ConflictFinder;
-import ch.geomo.tramaps.geo.util.GeomUtil;
-import ch.geomo.tramaps.graph.Edge;
-import ch.geomo.tramaps.graph.Node;
-import ch.geomo.tramaps.graph.Route;
 import ch.geomo.tramaps.map.MetroMap;
-import com.vividsolutions.jts.geom.Coordinate;
+import ch.geomo.tramaps.test.SimpleExampleMetroMap;
 import com.vividsolutions.jts.geom.Envelope;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -20,8 +16,6 @@ import javafx.scene.shape.StrokeLineCap;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Set;
 
 public class MainScaleApp extends Application {
@@ -37,7 +31,7 @@ public class MainScaleApp extends Application {
         this.stage = primaryStage;
         this.stage.setTitle("Tramaps GUI");
         this.initLayout();
-        this.createTestMap();
+        this.map = new SimpleExampleMetroMap();
         this.scale();
         this.draw();
     }
@@ -51,15 +45,15 @@ public class MainScaleApp extends Application {
         Group group = new Group();
         Envelope bbox = this.map.getBoundingBox();
 
-        Canvas canvas = new Canvas(bbox.getWidth()+100, bbox.getHeight()+100);
+        Canvas canvas = new Canvas(bbox.getWidth() + 100, bbox.getHeight() + 100);
         GraphicsContext context = canvas.getGraphicsContext2D();
 
         this.drawMetroMap(context, bbox);
 
         // hack -> to be removed
         group.setRotate(270);
-        canvas.setTranslateX(bbox.getMinY()/2);
-        canvas.setTranslateY(-bbox.getMinX()/2);
+        canvas.setTranslateX(bbox.getMinY() / 2);
+        canvas.setTranslateY(-bbox.getMinX() / 2);
 
         group.getChildren().add(canvas);
 
@@ -71,7 +65,7 @@ public class MainScaleApp extends Application {
     private void drawMetroMap(GraphicsContext context, Envelope bbox) {
 
         // start drawing at the top left
-        context.translate(-bbox.getMinX()+50, -bbox.getMinY()+50);
+        context.translate(-bbox.getMinX() + 50, -bbox.getMinY() + 50);
 
         this.map.getEdges().forEach(edge -> {
             double width = edge.getEdgeWidth(this.routeMargin);
@@ -108,59 +102,7 @@ public class MainScaleApp extends Application {
 
     }
 
-    public void createTestMap() {
-
-        this.map = new MetroMap();
-
-        Node a = new Node(GeomUtil.createPoint(new Coordinate(150, 200)));
-        Node b = new Node(GeomUtil.createPoint(new Coordinate(150, 100)));
-        Node c = new Node(GeomUtil.createPoint(new Coordinate(200, 100)));
-        Node d = new Node(GeomUtil.createPoint(new Coordinate(200, 150)));
-        Node e = new Node(GeomUtil.createPoint(new Coordinate(200, 250)));
-        Node f = new Node(GeomUtil.createPoint(new Coordinate(150, 300)));
-        Node g = new Node(GeomUtil.createPoint(new Coordinate(100, 300)));
-        Node h = new Node(GeomUtil.createPoint(new Coordinate(100, 200)));
-        Node i = new Node(GeomUtil.createPoint(new Coordinate(100, 150)));
-
-        Edge ab = new Edge(a, b);
-        Edge bc = new Edge(b, c);
-        Edge cd = new Edge(c, d);
-        Edge de = new Edge(d, e);
-        Edge ef = new Edge(e, f);
-        Edge fg = new Edge(f, g);
-        Edge gh = new Edge(g, h);
-        Edge ha = new Edge(h, a);
-        Edge hi = new Edge(h, i);
-        Edge ib = new Edge(i, b);
-        Edge ia = new Edge(i, a);
-
-        Route line1 = new Route(20, Color.BLUE);
-        Route line2 = new Route(20, Color.RED);
-        Route line3 = new Route(20, Color.GREEN);
-        Route line4 = new Route(20, Color.YELLOW);
-        Route line5 = new Route(20, Color.ORANGE);
-        Route line6 = new Route(20, Color.MAGENTA);
-        Route line7 = new Route(20, Color.BLACK);
-
-        ab.setRoutes(Arrays.asList(line1, line2, line3, line4, line5));
-        bc.setRoutes(Arrays.asList(line1, line2, line4, line5));
-        cd.setRoutes(Arrays.asList(line1, line2, line4, line5));
-        de.setRoutes(Arrays.asList(line1, line2, line4, line5));
-        ef.setRoutes(Arrays.asList(line1, line2, line4, line5, line6));
-        fg.setRoutes(Arrays.asList(line1, line6));
-        gh.setRoutes(Arrays.asList(line1, line6));
-        ha.setRoutes(Arrays.asList(line1, line2, line3, line6, line7));
-        hi.setRoutes(Arrays.asList(line1, line3, line6));
-        ib.setRoutes(Collections.singletonList(line6));
-        ia.setRoutes(Arrays.asList(line1, line4, line5));
-
-        this.map.getNodes().addAll(Arrays.asList(a, b, c, d, e, f, g, h, i));
-        this.map.getEdges().addAll(Arrays.asList(ab, bc, cd, de, ef, fg, gh, ha, hi, ib, ia));
-
-    }
-
     public void scale() {
-
         DisplacementHandler handler = new DisplacementHandler();
         System.out.println("Before Scaling:");
         System.out.println(map);
@@ -168,7 +110,20 @@ public class MainScaleApp extends Application {
         System.out.println("Scaled Map:");
         System.out.println(map);
         System.out.println("Finish");
+    }
 
+    public void scaleSubGraphs() {
+        // TODO
+    }
+
+    public void displace() {
+        DisplacementHandler handler = new DisplacementHandler();
+        System.out.println("Before Displacement:");
+        System.out.println(map);
+        handler.makeSpaceByDisplacement(map, this.routeMargin, this.edgeMargin);
+        System.out.println("Modified Map:");
+        System.out.println(map);
+        System.out.println("Finish");
     }
 
     public static void main(String[] args) throws IOException {
---
+++ src/main/java/ch/geomo/tramaps/test/SimpleExampleMetroMap.java
@@ -0,0 +1,65 @@
+package ch.geomo.tramaps.test;
+
+import ch.geomo.tramaps.geo.util.GeomUtil;
+import ch.geomo.tramaps.graph.Edge;
+import ch.geomo.tramaps.graph.Node;
+import ch.geomo.tramaps.graph.Route;
+import ch.geomo.tramaps.map.MetroMap;
+import com.vividsolutions.jts.geom.Coordinate;
+import javafx.scene.paint.Color;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SimpleExampleMetroMap extends MetroMap {
+
+    public SimpleExampleMetroMap() {
+
+        Node a = new Node(GeomUtil.createPoint(new Coordinate(150, 200)));
+        Node b = new Node(GeomUtil.createPoint(new Coordinate(150, 100)));
+        Node c = new Node(GeomUtil.createPoint(new Coordinate(200, 100)));
+        Node d = new Node(GeomUtil.createPoint(new Coordinate(200, 150)));
+        Node e = new Node(GeomUtil.createPoint(new Coordinate(200, 250)));
+        Node f = new Node(GeomUtil.createPoint(new Coordinate(150, 300)));
+        Node g = new Node(GeomUtil.createPoint(new Coordinate(100, 300)));
+        Node h = new Node(GeomUtil.createPoint(new Coordinate(100, 200)));
+        Node i = new Node(GeomUtil.createPoint(new Coordinate(100, 150)));
+
+        Edge ab = new Edge(a, b);
+        Edge bc = new Edge(b, c);
+        Edge cd = new Edge(c, d);
+        Edge de = new Edge(d, e);
+        Edge ef = new Edge(e, f);
+        Edge fg = new Edge(f, g);
+        Edge gh = new Edge(g, h);
+        Edge ha = new Edge(h, a);
+        Edge hi = new Edge(h, i);
+        Edge ib = new Edge(i, b);
+        Edge ia = new Edge(i, a);
+
+        Route line1 = new Route(20, Color.BLUE);
+        Route line2 = new Route(20, Color.RED);
+        Route line3 = new Route(20, Color.GREEN);
+        Route line4 = new Route(20, Color.YELLOW);
+        Route line5 = new Route(20, Color.ORANGE);
+        Route line6 = new Route(20, Color.MAGENTA);
+        Route line7 = new Route(20, Color.BLACK);
+
+        ab.setRoutes(Arrays.asList(line1, line2, line3, line4, line5));
+        bc.setRoutes(Arrays.asList(line1, line2, line4, line5));
+        cd.setRoutes(Arrays.asList(line1, line2, line4, line5));
+        de.setRoutes(Arrays.asList(line1, line2, line4, line5));
+        ef.setRoutes(Arrays.asList(line1, line2, line4, line5, line6));
+        fg.setRoutes(Arrays.asList(line1, line6));
+        gh.setRoutes(Arrays.asList(line1, line6));
+        ha.setRoutes(Arrays.asList(line1, line2, line3, line6, line7));
+        hi.setRoutes(Arrays.asList(line1, line3, line6));
+        ib.setRoutes(Collections.singletonList(line6));
+        ia.setRoutes(Arrays.asList(line1, line4, line5));
+
+        this.getNodes().addAll(Arrays.asList(a, b, c, d, e, f, g, h, i));
+        this.getEdges().addAll(Arrays.asList(ab, bc, cd, de, ef, fg, gh, ha, hi, ib, ia));
+
+    }
+
+}
